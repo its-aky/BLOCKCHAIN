8246b8be2e18913e0de1daf2b07e2b00139fef28
its-aky <kumaramityadav230@gmail.com>
2023-10-26 07:17:43 +0530

    Added Interface code from github

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -7,6 +7,55 @@ pragma solidity >=0.6.0 <0.9.0;
 //Interfaces compile down to an ABI(Application Binary Interface)
 //ABI tells solidity and other programming languages how it can interact with a contract
 
+interface AggregatorV3Interface {
+
+  function decimals()
+    external
+    view
+    returns (
+      uint8
+    );
+
+  function description()
+    external
+    view
+    returns (
+      string memory
+    );
+
+  function version()
+    external
+    view
+    returns (
+      uint256
+    );
+
+  function getRoundData(
+    uint80 _roundId
+  )
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
+
+}
+
 contract FundMe {
 
     //store the address which sent us amount also store the amount
